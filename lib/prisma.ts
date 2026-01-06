ea9f86abb6821f019ffc54d59a3f4b89deb50e93
Santa Claus <santaclaus@Santas-MacBook-Pro.local>
2026-01-06 23:56:18 +0300

    add PrismaNeon

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,8 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "./generated/prisma";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-const adapter = new PrismaBetterSqlite3({
-  url: "file:./dev.db",
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
 });
 
 export const prisma = new PrismaClient({ adapter });
