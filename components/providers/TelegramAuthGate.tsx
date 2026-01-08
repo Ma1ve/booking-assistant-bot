f4a58c9f9aa43433434a90ef01ed9e64b7ef6e1b
Santa Claus <santaclaus@Santas-MacBook-Pro.local>
2026-01-09 02:28:59 +0300

    another test

--- components/providers/TelegramAuthGate.tsx
+++ components/providers/TelegramAuthGate.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
-// import { retrieveRawInitData } from "@tma.js/sdk";
+import { retrieveRawInitData } from "@tma.js/sdk";
 import { Spinner } from "../ui/spinner";
 
 interface TelegramAuthValue {
@@ -23,39 +23,41 @@ export function TelegramAuthGate({ children }: { children: React.ReactNode }) {
   const [allowed, setAllowed] = useState<boolean | null>(null);
   const [isAdmin, setIsAdmin] = useState<boolean | null>(null);
   const [chatId, setChatId] = useState<string | null>(null);
-  //   useEffect(() => {
-  // const initData = retrieveRawInitData();
-  // fetch("/api/check-role", {
-  //   method: "POST",
-  //   headers: { "Content-Type": "application/json" },
-  //   body: JSON.stringify({ initData }),
-  // })
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     if (data.ok) {
-  //       setAllowed(true);
-  //       setIsAdmin(data.isAdmin);
-  //       console.log(data, "data");
-  //   setChatId
-  //       console.log("Доступ разрешён");
-  //     } else {
-  //       console.warn("Доступ запрещён:", data.error);
-  //     }
-  //   });
-  //   }, []);
 
-  //   if (allowed === null || isAdmin === null || chatId === null)
-  //     return (
-  //       <div className="flex justify-center items-center">
-  //         <Spinner className="size-15" />
-  //       </div>
-  //     );
+  useEffect(() => {
+    const initData = retrieveRawInitData();
+    fetch("/api/check-role", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ initData }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.ok) {
+          setAllowed(true);
+          setIsAdmin(data.isAdmin);
+          setChatId(data.user.id);
+          console.log(data, "data");
+          console.log("Доступ разрешён");
+        } else {
+          console.warn("Доступ запрещён:", data.error);
+        }
+      });
+  }, []);
 
   const telegramAuthValue = useMemo(() => {
-    return { isAdmin: false, chatId: "898529686" };
+    return { isAdmin: isAdmin, chatId: chatId };
   }, [isAdmin, chatId]);
 
+  if (allowed === null || isAdmin === null || chatId === null)
+    return (
+      <div className="flex justify-center items-center">
+        <Spinner className="size-15" />
+      </div>
+    );
+
   return (
+    // Поправить
     <TelegramAuthContext.Provider value={telegramAuthValue}>
       {children}
     </TelegramAuthContext.Provider>
