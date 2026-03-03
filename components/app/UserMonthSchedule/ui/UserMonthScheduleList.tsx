687ad7c03a56a0bbb54bfbd6a933ce93e1a72686
Santa Claus <santaclaus@Santas-MacBook-Pro.local>
2026-03-03 19:19:26 +0300

    add mt-5

--- components/app/UserMonthSchedule/ui/UserMonthScheduleList.tsx
+++ components/app/UserMonthSchedule/ui/UserMonthScheduleList.tsx
@@ -11,7 +11,7 @@ export const UserMonthScheduleList = ({ userScheduleList }: UserMonthScheduleLis
       <div className="absolute top-0 left-0 right-0 h-16 bg-gradient-to-b from-black/70 to-transparent pointer-events-none z-10" />
       <div className="flex flex-col gap-7 overflow-y-auto no-scrollbar firt:pt-10">
         <div className="flex justify-center">
-          <Badge className="mt-10 mb-3 text-md">Ближайшие записи:</Badge>
+          <Badge className="mt-5 mb-3 text-md">Ближайшие записи:</Badge>
         </div>
 
         {userScheduleList.map((schedule) => (
